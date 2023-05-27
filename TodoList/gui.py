01db96efa931dc2ec68514f707457e078c37540e
Amit Tambe <amittambe0000@gmail.com>
2023-05-27 11:47:09 +0530

    Implement Complete and exit buttons

--- TodoList/gui.py
+++ TodoList/gui.py
@@ -6,6 +6,8 @@ label=sg.Text('Type in a todo')
 input_box=sg.InputText(tooltip='Enter a todo',key='todo')
 add_button=sg.Button('Add')
 edit_button=sg.Button('Edit')
+complete_button=sg.Button('Complete')
+exit_button=sg.Button('Exit')
 list_box=sg.Listbox(values=functions.get_todos(),
                     key='todos',
                     size=[45,10],
@@ -14,7 +16,8 @@ list_box=sg.Listbox(values=functions.get_todos(),
 window=sg.Window('My todo app',layout=[
     [label],
     [input_box,add_button],
-    [list_box,edit_button]])
+    [list_box,edit_button,complete_button],
+    [exit_button]])
 
 while True:
     event,values =window.read()
@@ -27,12 +30,23 @@ while True:
         window['todos'].update(values=todos)
     elif(event=='Edit'):
         todo_to_edit=values['todos'][0]
+        print('todo is ',todo_to_edit)
         new_to_do=values['todo']
+        print('todo is ',new_to_do)
         todos=functions.get_todos()
         index=todos.index(todo_to_edit)
-        todos[index]=new_to_do+'\n'
+        todos[index]=new_to_do
         functions.write_todos(todos)
         window['todos'].update(values=todos)
+    elif(event=='Complete'):
+        to_do_delete=values['todos'][0]
+        todos=functions.get_todos()
+        todos.remove(to_do_delete)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        window['todo'].update(value='')
+    elif(event=='Exit'):
+        break
     elif(event=='todos'):
         window['todo'].update(value=values['todos'][0])
     elif(event==sg.WIN_CLOSED):
