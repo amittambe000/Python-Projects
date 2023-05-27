b7fd4004664f5f42d0d54be7a945b28529dd622f
Amit Tambe <amittambe0000@gmail.com>
2023-05-27 12:18:24 +0530

    change theme , add clock and handle errors

--- TodoList/gui.py
+++ TodoList/gui.py
@@ -1,8 +1,10 @@
 from modules import functions
 import PySimpleGUI as sg
+import time
 
 sg.theme('DarkAmber')
 label=sg.Text('Type in a todo')
+clock=sg.Text('',key='clock')
 input_box=sg.InputText(tooltip='Enter a todo',key='todo')
 add_button=sg.Button('Add')
 edit_button=sg.Button('Edit')
@@ -13,14 +15,15 @@ list_box=sg.Listbox(values=functions.get_todos(),
                     size=[45,10],
                     enable_events=True)
 
-window=sg.Window('My todo app',layout=[
+window=sg.Window('My todo app',layout=[[clock],
     [label],
     [input_box,add_button],
     [list_box,edit_button,complete_button],
     [exit_button]])
 
 while True:
-    event,values =window.read()
+    event,values =window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%b %d %Y %H:%M:%S"))
     print(event)
     print(values)
     if(event=='Add'):
@@ -29,22 +32,28 @@ while True:
         functions.write_todos(todos)
         window['todos'].update(values=todos)
     elif(event=='Edit'):
-        todo_to_edit=values['todos'][0]
-        print('todo is ',todo_to_edit)
-        new_to_do=values['todo']
-        print('todo is ',new_to_do)
-        todos=functions.get_todos()
-        index=todos.index(todo_to_edit)
-        todos[index]=new_to_do
-        functions.write_todos(todos)
-        window['todos'].update(values=todos)
+        try:   
+            todo_to_edit=values['todos'][0]
+            print('todo is ',todo_to_edit)
+            new_to_do=values['todo']
+            print('todo is ',new_to_do)
+            todos=functions.get_todos()
+            index=todos.index(todo_to_edit)
+            todos[index]=new_to_do
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        except:
+            sg.popup("Please select an item first")
     elif(event=='Complete'):
-        to_do_delete=values['todos'][0]
-        todos=functions.get_todos()
-        todos.remove(to_do_delete)
-        functions.write_todos(todos)
-        window['todos'].update(values=todos)
-        window['todo'].update(value='')
+        try:
+            to_do_delete=values['todos'][0]
+            todos=functions.get_todos()
+            todos.remove(to_do_delete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        except:
+            sg.popup("Please select an item first")
     elif(event=='Exit'):
         break
     elif(event=='todos'):
