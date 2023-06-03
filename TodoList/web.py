3c6ad265a3db56f89534fadd7047e2e94b768ae5
Amit Tambe <amittambe0000@gmail.com>
2023-06-03 10:30:10 +0530

    Added functionality to checkbox

--- TodoList/web.py
+++ TodoList/web.py
@@ -14,8 +14,14 @@ st.write("This app is to improve your productivity")
 
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox=st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
 
 st.text_input(label='',placeholder='Add new todo...',
               on_change=add_todo,key='new_todo')
